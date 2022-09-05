0edd63848ea2d49bc12fde8cb0e04939afeeeac1
Sungjun Kim <rlatjdwns8934@gmail.com>
2022-09-05 11:11:58 +0900

    first initial

---
+++ src/main/java/com/sparta/miniproject/configuration/JwtSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.sparta.miniproject.configuration;
+
+import com.sparta.miniproject.jwt.JwtFilter;
+import com.sparta.miniproject.jwt.TokenProvider;
+import com.sparta.miniproject.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfiguration
+        extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final String SECRET_KEY;
+    private final TokenProvider tokenProvider;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) {
+        JwtFilter customJwtFilter = new JwtFilter(SECRET_KEY, tokenProvider, userDetailsService);
+        httpSecurity.addFilterBefore(customJwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
